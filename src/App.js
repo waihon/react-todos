18bfc989a6a5bd9f43481202a72e455253e042bf
Waihon Yew <yewwaihon@gmail.com>
2024-03-28 16:11:43 +0800

    Add and implement onClick listener

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ function App() {
     { rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "Use One" },
   ]
 
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three"
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className="mt-5 container">
       <div className="card">
@@ -18,6 +30,9 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
