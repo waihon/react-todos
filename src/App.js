5e0213c05365e59e47e94651341b646873967f98
Waihon Yew <yewwaihon@gmail.com>
2024-03-28 18:47:59 +0800

    Pass functions through props

--- src/App.js
+++ src/App.js
@@ -12,15 +12,14 @@ function App() {
     { rowNumber: 4, rowDescription: "Charge phone battery", rowAssigned: "Use One" },
   ]);
 
-  const addTodo = () => {
+  const addTodo = (description, assigned) => {
     if (todos.length > 0) {
       const newTodo = {
         rowNumber: todos.length + 1,
-        rowDescription: "New Todo",
-        rowAssigned: "User Three"
+        rowDescription: description,
+        rowAssigned: assigned
       };
       setTodos(todos => [...todos, newTodo]);
-      console.log(todos);
     }
   }
 
@@ -32,10 +31,7 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos} />
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
-          </button>
-          <NewTodoForm />
+          <NewTodoForm addTodo={addTodo} />
         </div>
       </div>
     </div>
--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
 
-function NewTodoForm() {
+function NewTodoForm({ addTodo }) {
 
   const [description, setDescription] = useState('');
   const [assigned, setAssigned] = useState('');
 
+  const submitTodo = () => {
+    if (description !== '' && assigned !== '') {
+      addTodo(description, assigned);
+      setDescription('');
+      setAssigned('');
+    }
+  };
+
   return (
     <div className='mt-5'>
       <form>
-        <div className='mb-3'>
-          <label className='form-label'>Assigned</label>
-          <input
-            type='text'
-            className='form-control'
-            required
-            onChange={e => setAssigned(e.target.value)}
-            value={assigned}
-          />
-        </div>
         <div className='mb-3'>
           <label className='form-label'>Description</label>
           <textarea
@@ -28,7 +26,23 @@ function NewTodoForm() {
             value={description}
           />
         </div>
-        <button type='button' className='btn btn-primary'>Add Todo</button>
+        <div className='mb-3'>
+          <label className='form-label'>Assigned</label>
+          <input
+            type='text'
+            className='form-control'
+            required
+            onChange={e => setAssigned(e.target.value)}
+            value={assigned}
+          />
+        </div>
+        <button
+          type='button'
+          className='btn btn-primary'
+          onClick={submitTodo}
+        >
+            Add todo
+          </button>
       </form>
     </div>
   )
