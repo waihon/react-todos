9409f65c206bbe7d360dc0d4ad5b363ab3f2f51a
Waihon Yew <yewwaihon@gmail.com>
2024-03-28 16:57:54 +0800

    Create todo input form

---
+++ src/components/NewTodoForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NewTodoForm() {
+  return (
+    <div className='mt-5'>
+      <form>
+        <div className='mb-3'>
+          <label className='form-label'>Assigned</label>
+          <input type='text' className='form-control' required></input>
+        </div>
+        <div className='mb-3'>
+          <label className='form-label'>Description</label>
+          <textarea className='form-control' rows={3}></textarea>
+        </div>
+        <button type='button' className='btn btn-primary'>Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewTodoForm;
