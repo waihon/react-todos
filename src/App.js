31d8e7ef413c413b72f40a2280dbdd22b7899247
Waihon Yew <yewwaihon@gmail.com>
2024-03-28 10:34:29 +0800

    Create a table of todos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          We made our first React application!
-        </p>
-      </header>
+    <div>
+      <div>
+        <div>
+          Your Todo's
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
